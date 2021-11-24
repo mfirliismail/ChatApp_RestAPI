4c44d8b06228d420ae0d809f0db973bb75c46abc
mfirliismail <m.firliismail@gmail.com>
2021-11-24 13:55:03 +0700

    fix

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,6 @@ const userRoute = require('./users')
 const messageRoute = require('./message')
 
 router.use('/users', userRoute)
-router.use('/message', messageRoute)
+router.use('/messages', messageRoute)
 
 module.exports = router
--- routes/message.js
+++ routes/message.js
@@ -4,6 +4,8 @@ const { authToken } = require('../middlewares/auth')
 
 
 router.post('/send/:sendId', authToken, messageController.sendMessage)
+router.get('/all', authToken, messageController.getAllConversations)
+router.get('/user/:partner', authToken, messageController.getAllMessageInOneConversation)
 
 
 module.exports = router
